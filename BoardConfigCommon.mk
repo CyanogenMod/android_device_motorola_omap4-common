d486de6d676cc36ebd632e91bcd5847898477e8e
dhacker29 <davidhackerdvm@gmail.com>
2012-08-28 04:45:48 -0500

    initial commit
    Move common items for mapphone devices to central location

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,158 @@
+# define OMAP_ENHANCEMENT variables
+include device/motorola/common/Config.mk
+
+# Camera
+USE_CAMERA_STUB := false
+BOARD_USES_TI_CAMERA_HAL := true
+TI_CAMERAHAL_DEBUG_ENABLED := true
+
+OMAP_ENHANCEMENT_MULTIGPU := true
+
+ENHANCED_DOMX := true
+#USE_ITTIAM_AAC := true
+#BLTSVILLE_ENHANCEMENT :=true
+
+# inherit from the proprietary version
+-include vendor/motorola/common/BoardConfigVendor.mk
+
+# Processor
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := omap4
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Module Build
+
+WLAN_MODULES:
+	make clean -C hardware/ti/wlan/mac80211/compat_wl12xx
+	make -j8 -C hardware/ti/wlan/mac80211/compat_wl12xx KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_spi.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
+
+TARGET_KERNEL_MODULES += WLAN_MODULES
+
+# Storage / Sharing
+BOARD_VOLD_MAX_PARTITIONS := 100
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
+BOARD_MTP_DEVICE := "/dev/mtp"
+
+# Connectivity - Wi-Fi
+USES_TI_MAC80211 := true
+ifdef USES_TI_MAC80211
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
+BOARD_HOSTAPD_DRIVER             := NL80211
+PRODUCT_WIRELESS_TOOLS           := true
+BOARD_WLAN_DEVICE                := wl12xx_mac80211
+BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER             := ""
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
+endif
+
+# adb has root
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := true
+BUILD_WITH_ALSA_UTILS := true
+HAVE_2_3_DSP := 1
+TARGET_PROVIDES_LIBAUDIO := true
+BOARD_USE_MOTO_DOCK_HACK := true
+COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Recovery
+BUILD_BOOTMENU_STANDALONE := true
+#BOARD_HAS_LOCKED_BOOTLOADER := true
+BOARD_HAS_WEBTOP := true
+TARGET_PREBUILT_RECOVERY_KERNEL := device/motorola/common/recovery-kernel
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_UMS_LUNFILE := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
+BOARD_ALWAYS_INSECURE := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_MKE2FS := device/motorola/common/releaseutils/mke2fs
+BOARD_HAS_SDCARD_INTERNAL := true
+#BOARD_HAS_SDEXT := false
+TARGET_RECOVERY_PRE_COMMAND := "echo 1 > /data/.recovery_mode; sync;"
+TARGET_RECOVERY_PRE_COMMAND_CLEAR_REASON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+
+
+# Graphics
+BOARD_EGL_CFG := device/motorola/common/prebuilt/etc/egl.cfg
+USE_OPENGL_RENDERER := true
+
+ifdef OMAP_ENHANCEMENT
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP4
+ifdef USE_ITTIAM_AAC
+COMMON_GLOBAL_CFLAGS += -DUSE_ITTIAM_AAC
+endif
+ifdef OMAP_ENHANCEMENT_S3D
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_S3D
+endif
+ifdef OMAP_ENHANCEMENT_CPCAM
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_CPCAM
+endif
+ifdef OMAP_ENHANCEMENT_VTC
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_VTC
+endif
+endif
+
+# MOTOROLA
+USE_MOTOROLA_CODE := true
+ifdef USE_MOTOROLA_CODE
+COMMON_GLOBAL_CFLAGS += -DUSE_MOTOROLA_CODE
+endif
+USE_MOTOROLA_USERS := true
+ifdef USE_MOTOROLA_USERS
+COMMON_GLOBAL_CFLAGS += -DUSE_MOTOROLA_USERS
+endif
+
+# Media / Radio
+# Off currently
+
+# OTA Packaging
+TARGET_PROVIDES_RELEASETOOLS := true
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/motorola/common/releasetools/common_ota_from_target_files
+TARGET_CUSTOM_RELEASETOOL := ./vendor/motorola/common/tools/squisher
+
+# CodeAurora Optimizations: msm8960: Improve performance of memmove, bcopy, and memmove_words
+# added by twa_priv
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_KRAIT_BIONIC_PLDOFFS := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH := 64
+TARGET_KRAIT_BIONIC_PLDSIZE := 64
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+
+# Misc.
+BOARD_USE_BATTERY_CHARGE_COUNTER := true
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_NEEDS_CUTILS_LOG := true
+BOARD_USES_SECURE_SERVICES := true
+BOARD_HAS_MAPPHONE_SWITCH := true
+USE_IPV6_ROUTE := true
+LEGACY_RIL := true
+
+# Common device independent definitions
+include device/ti/common-open/BoardConfig.mk
---
+++ common.mk
@@ -0,0 +1,145 @@
+#
+# This is the product configuration for a full common
+#
+
+DEVICE_FOLDER := device/motorola/common
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+
+# Device overlay
+    DEVICE_PACKAGE_OVERLAYS += $(DEVICE_FOLDER)/overlay
+
+# high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_PACKAGES := \
+    charger \
+    charger_res_images
+
+# Hardware HALs
+PRODUCT_PACKAGES += \
+    camera.omap4 \
+    libinvensense_mpl \
+    libedid
+
+PRODUCT_PACKAGES += \
+    libaudioutils \
+    libaudiohw_legacy
+
+# BlueZ a2dp Audio HAL module
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+# BlueZ test tools
+PRODUCT_PACKAGES += \
+    hciconfig \
+    hcitool
+
+# Modem
+PRODUCT_PACKAGES += \
+    libaudiomodemgeneric \
+    rild \
+    radiooptions
+
+# Wifi
+PRODUCT_PACKAGES += \
+    lib_driver_cmd_wl12xx \
+    dhcpcd.conf \
+    hostapd.conf \
+    wifical.sh \
+    wpa_supplicant.conf \
+    TQS_D_1.7.ini \
+    TQS_D_1.7_127x.ini \
+    crda \
+    regulatory.bin \
+    calibrator \
+    busybox
+
+# Wifi Direct and WPAN
+PRODUCT_PACKAGES += \
+    ti_wfd_libs \
+    ti-wpan-fw
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    bt_sco_app \
+    uim-sysfs 
+
+# Release utilities
+PRODUCT_PACKAGES += \
+    common_releaseutils-check_kernel \
+    common_releaseutils-finalize_release \
+    common_releaseutils-mke2fs \
+    common_releaseutils-tune2fs
+
+PRODUCT_PACKAGES += \
+    evtest \
+    camera_test \
+    Camera \
+    CameraOMAP4 \
+    Superuser \
+    su \
+    DockAudio \
+    parse_hdmi_edid
+
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory \
+    FileManager \
+    MusicFX \
+    Apollo
+
+# Permissions files
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+# Prebuilts
+PRODUCT_COPY_FILES += \
+    $(DEVICE_FOLDER)/prebuilt/bin/strace:system/bin/strace
+
+# Phone settings
+PRODUCT_COPY_FILES += \
+    $(DEVICE_FOLDER)/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Kexec files
+ifneq ($(TARGET_DEVICE),solana)
+PRODUCT_COPY_FILES += \
+    $(DEVICE_FOLDER)/kexec/arm_kexec.ko:system/etc/kexec/arm_kexec.ko \
+    $(DEVICE_FOLDER)/kexec/atags:system/etc/kexec/atags \
+    $(DEVICE_FOLDER)/kexec/kexec:system/etc/kexec/kexec \
+    $(DEVICE_FOLDER)/kexec/kexec.ko:system/etc/kexec/kexec.ko \
+    $(DEVICE_FOLDER)/kexec/uart.ko:system/etc/kexec/uart.ko
+endif
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# still need to set english for audio init
+PRODUCT_LOCALES += en_US
+
+# stuff specific to ti OMAP4 hardware
+$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
+$(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
+$(call inherit-product, device/ti/proprietary-open/wl12xx/wlan/wl12xx-wlan-fw-products.mk)
+$(call inherit-product-if-exists, vendor/motorola/common/proprietary/imgtec/sgx-imgtec-bins.mk)
+$(call inherit-product-if-exists, vendor/motorola/common/proprietary/custom-omap4xxx/custom-omap4.mk)
+$(call inherit-product-if-exists, vendor/motorola/common/common-vendor.mk)
