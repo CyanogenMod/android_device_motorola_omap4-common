7b08a83609fe9a0992a1329fabc0d827641756d4
Hashcode <hashcode0f@gmail.com>
2012-12-16 22:58:35 -0800

    common motorola omap4 libion for stock-based kernel builds
    Change-Id: I765eddefd41cf2be3ceb8dea506f785f23242d78

---
+++ libion/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := ion.c
+LOCAL_MODULE := libion_ti
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := liblog
+include $(BUILD_SHARED_LIBRARY)
+
