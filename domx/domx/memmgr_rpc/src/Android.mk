20fbc63e631b06a2795b684e2abf6a2c7308b5f7
Hashcode <hashcode0f@gmail.com>
2012-12-16 22:57:31 -0800

    common motorola omap4 domx for stock-based kernel builds
    Change-Id: I6ec351ddbfc8297036ce1180e97a4d3187136c2a

---
+++ domx/domx/memmgr_rpc/src/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	memmgr_rpc.c
+
+LOCAL_C_INCLUDES += \
+	$(LOCAL_PATH)/../inc \
+	$(LOCAL_PATH)/../../ \
+	$(LOCAL_PATH)/../../omx_rpc/inc \
+	$(LOCAL_PATH)/../../../domx
+
+LOCAL_CFLAGS += -D_Android
+
+LOCAL_SHARED_LIBRARIES := \
+	libOMX_CoreOsal \
+	libipcutils \
+	libsysmgr \
+	libipc \
+	librcm \
+	libnotify \
+	libc \
+	liblog    \
+	libd2cmap
+
+LOCAL_MODULE:= libmemmgr_rpc
+
+include $(BUILD_SHARED_LIBRARY)
