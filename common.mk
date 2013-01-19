294855305360089041898f5d5c957a5c9f66dd20
dhacker29 <davidhackerdvm@gmail.com>
2013-01-18 18:18:01 -0600

    Move dalvik heap define to device

--- common.mk
+++ common.mk
@@ -331,4 +331,3 @@ PRODUCT_LOCALES += en_US
 #$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 $(call inherit-product, hardware/ti/omap4xxx/security/Android.mk)
 $(call inherit-product-if-exists, vendor/motorola/common/common-vendor.mk)
-$(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
