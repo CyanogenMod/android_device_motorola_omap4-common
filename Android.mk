5474a5005cdefdda1881b5810ba056e2c8b39ced
Hashcode <hashcode0f@gmail.com>
2013-03-11 22:38:59 -0700

    create symlink for gralloc
    Change-Id: Ie51f7e524dc8821a5764a019d718292c2da8c762

--- Android.mk
+++ Android.mk
@@ -13,5 +13,22 @@
 # limitations under the License.
 
 ifeq ($(BOARD_VENDOR),motorola-omap4)
-    include $(all-subdir-makefiles)
+
+LOCAL_PATH := $(call my-dir)
+
+#Creating Gralloc SymLink
+GRALLOC_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw/gralloc.omap4.so
+$(GRALLOC_SYMLINK): GRALLOC_FILE := gralloc.omap4430.so
+$(GRALLOC_SYMLINK): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(GRALLOC_FILE)"
+	@rm -rf $@
+	$(hide) ln -fs $(GRALLOC_FILE) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GRALLOC_SYMLINK)
+
+# for mm/mmm
+all_modules: $(GRALLOC_SYMLINK)
+
+include $(all-subdir-makefiles)
+
 endif
