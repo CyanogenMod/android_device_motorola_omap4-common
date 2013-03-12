780d87f93f2b64cccde5e82e4b47d678fb798810
Hashcode <hashcode0f@gmail.com>
2013-03-11 22:33:50 -0700

    Add PVR module source
    Change-Id: Ic83baae9f58d81429df9df31d71eaf3f51179613

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,6 +23,8 @@
 # set to allow building from common
 BOARD_VENDOR := motorola-omap4
 
+COMMON_FOLDER := device/motorola/omap4-common
+
 # includes fix for framebuffer
 TARGET_SPECIFIC_HEADER_PATH := device/motorola/omap4-common/include
 
@@ -83,6 +85,16 @@ WLAN_MODULES:
 	$(ARM_EABI_TOOLCHAIN)/arm-eabi-strip --strip-unneeded $(KERNEL_MODULES_OUT)/wl12xx_sdio.ko
 
 TARGET_KERNEL_MODULES += WLAN_MODULES
+
+# External SGX Module
+SGX_MODULES:
+	make clean -C $(COMMON_FOLDER)/pvr-source/eurasiacon/build/linux2/omap4430_android
+	cp $(TARGET_KERNEL_SOURCE)/drivers/video/omap2/omapfb/omapfb.h $(KERNEL_OUT)/drivers/video/omap2/omapfb/omapfb.h
+	make -j8 -C $(COMMON_FOLDER)/pvr-source/eurasiacon/build/linux2/omap4430_android ARCH=arm KERNEL_CROSS_COMPILE=arm-eabi- CROSS_COMPILE=arm-eabi- KERNELDIR=$(KERNEL_OUT) TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
+	mv $(KERNEL_OUT)/../../target/kbuild/pvrsrvkm_sgx540_120.ko $(KERNEL_MODULES_OUT)
+	$(ARM_EABI_TOOLCHAIN)/arm-eabi-strip --strip-unneeded $(KERNEL_MODULES_OUT)/pvrsrvkm_sgx540_120.ko
+
+TARGET_KERNEL_MODULES += SGX_MODULES
 
 # Storage / Sharing
 BOARD_VOLD_MAX_PARTITIONS := 100
