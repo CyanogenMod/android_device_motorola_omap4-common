4263111e8cac1719df5dfbc37a0e1aa0d4d3c54d
Hashcode <hashcode0f@gmail.com>
2013-03-14 01:35:45 -0700

    libtiutils: update from omapzoom for jb, and rename to avoid conflict
    Change-Id: I5d67c6d43c2e0329f625a4e7c8990ceda13297a6

---
+++ libtiutils/Android.mk
@@ -0,0 +1,53 @@
+################################################
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_SRC_FILES:= \
+    DebugUtils.cpp \
+    MessageQueue.cpp \
+    Semaphore.cpp \
+    ErrorUtils.cpp
+
+LOCAL_SHARED_LIBRARIES:= \
+    libdl \
+    libui \
+    libbinder \
+    libutils \
+    libcutils
+
+LOCAL_C_INCLUDES += \
+    frameworks/native/include
+
+LOCAL_C_INCLUDES += \
+    bionic/libc/include 
+
+ifdef ENHANCED_DOMX
+LOCAL_C_INCLUDES += \
+    $(DOMX_PATH)/omx_core/inc \
+    $(DOMX_PATH)/mm_osal/inc
+else
+LOCAL_C_INCLUDES += \
+    hardware/ti/omap4xxx/domx/omx_core/inc \
+    hardware/ti/omap4xxx/domx/mm_osal/inc
+endif
+
+LOCAL_CFLAGS += -fno-short-enums $(ANDROID_API_CFLAGS)
+
+ifdef TI_UTILS_MESSAGE_QUEUE_DEBUG_ENABLED
+    # Enable debug logs
+    LOCAL_CFLAGS += -DMSGQ_DEBUG
+endif
+
+ifdef TI_UTILS_MESSAGE_QUEUE_DEBUG_FUNCTION_NAMES
+    # Enable function enter/exit logging
+    LOCAL_CFLAGS += -DTI_UTILS_FUNCTION_LOGGER_ENABLE
+endif
+
+LOCAL_MODULE:= libtiutils_custom
+LOCAL_MODULE_TAGS:= optional
+
+include $(BUILD_SHARED_LIBRARY)
