1b768800aab8a427853286904745705b12f0cc7e
dhacker29 <davidhackerdvm@gmail.com>
2013-04-01 01:44:06 -0500

    PDS backup and wifi MAC calibration
    Adapted from Quarx2K defy implementation and others
    
    Change-Id: I038cc2b453b22c13a4005e3f96e51172566d04a6

--- common.mk
+++ common.mk
@@ -20,7 +20,10 @@ COMMON_FOLDER := device/motorola/omap4-common
 
 # The gps config appropriate for this device
 PRODUCT_COPY_FILES += \
-    $(COMMON_FOLDER)/prebuilt/etc/gps.conf:system/etc/gps.conf
+    $(COMMON_FOLDER)/prebuilt/bin/pdsbackup.sh:system/bin/pdsbackup.sh \
+    $(COMMON_FOLDER)/prebuilt/bin/wifical.sh:system/bin/wifical.sh \
+    $(COMMON_FOLDER)/prebuilt/etc/gps.conf:system/etc/gps.conf \
+    $(COMMON_FOLDER)/prebuilt/etc/wifi/wlan_fem.ini:system/etc/wifi/wlan_fem.ini
 
 # Device overlay
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_FOLDER)/overlay
@@ -238,7 +241,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
     softap.interface=wlan0 \
     wifi.ap.interface=wlan0 \
-    wifi.supplicant_scan_interval=90
+    wifi.supplicant_scan_interval=90 \
+    persist.wlan.ti.calibrated=0
 
 # Define SDcard for use with CMUpdater
 PRODUCT_PROPERTY_OVERRIDES += \
