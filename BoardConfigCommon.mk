151157cbc78c3e6a9b5847aafc3367f319f17a13
dhacker29 <davidhackerdvm@gmail.com>
2013-05-31 05:15:45 -0500

    Use shared mapphone_mmi_defconfig and variant for differences
    Change-Id: Ie7f5c8bcc25a48f3d0ceaaad8b3cb5f0b566ebfa

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,7 +68,8 @@ TARGET_KRAIT_BIONIC_PLDSIZE := 64
 
 # Kernel/Module Build
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
-#TARGET_PROVIDES_INIT_RC := true
+TARGET_KERNEL_SOURCE := kernel/motorola/omap4-common
+TARGET_KERNEL_CONFIG := mapphone_mmi_defconfig
 
 WLAN_MODULES:
 	make clean -C hardware/ti/wlan/mac80211/compat_wl12xx
