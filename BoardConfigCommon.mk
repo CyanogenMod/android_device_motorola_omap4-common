4646ccc62e46256af26f100d91aec00295f349f4
dhacker29 <davidhackerdvm@gmail.com>
2013-08-09 19:26:57 -0500

    sepolicy: Fix some denials
    Change-Id: I20032d76faf655547e158dfe5f3030c0b1a66baf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -232,15 +232,16 @@ BOARD_SEPOLICY_DIRS += \
         device/motorola/omap4-common/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-        file_contexts \
-        device.te \
-        dhcp.te \
-        domain.te \
-        file.te \
-        init_shell.te \
-        mediaserver.te \
-        pvrsrvinit.te \
-        rild.te \
-        system.te \
-        tee.te \
-        vold.te
+    debuggered.te \
+    file_contexts \
+    device.te \
+    dhcp.te \
+    domain.te \
+    file.te \
+    init_shell.te \
+    mediaserver.te \
+    pvrsrvinit.te \
+    rild.te \
+    system.te \
+    tee.te \
+    vold.te
