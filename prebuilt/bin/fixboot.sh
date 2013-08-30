192cd7541af48eff50cc168c1c11980f117ea609
Hashcode <hashcode0f@gmail.com>
2013-08-30 00:03:14 -0700

    update prebuilts
    Change-Id: Ib7b890a764375342810a1165b3708d122cfc2c06

--- prebuilt/bin/fixboot.sh
+++ prebuilt/bin/fixboot.sh
@@ -1,34 +1,37 @@
+#!/sbin/bbx sh
+
 # remount root as rw
 /sbin/bbx mount -o remount,rw rootfs
 /sbin/bbx mkdir /ss
 /sbin/bbx chmod 777 /ss
 
 # mount safestrap partition
-/sbin/bbx mount -t vfat /dev/block/mmcblk1p25 /ss
+/sbin/bbx mount -t vfat /dev/block/emstorage /ss
 
+SLOT_LOC=$(/sbin/bbx cat /ss/safestrap/active_slot)
+
+if [ "$SLOT_LOC" != "stock" ]; then
 # create SS loopdevs
 /sbin/bbx mknod -m600 /dev/block/loop-system b 7 99
 /sbin/bbx mknod -m600 /dev/block/loop-userdata b 7 98
 /sbin/bbx mknod -m600 /dev/block/loop-cache b 7 97
+
+# setup loopbacks
+/sbin/bbx losetup /dev/block/loop-system /ss/safestrap/$SLOT_LOC/system.img
+/sbin/bbx losetup /dev/block/loop-userdata /ss/safestrap/$SLOT_LOC/userdata.img
+/sbin/bbx losetup /dev/block/loop-cache /ss/safestrap/$SLOT_LOC/cache.img
 
 # move real partitions out of the way
 /sbin/bbx mv /dev/block/system /dev/block/systemorig
 /sbin/bbx mv /dev/block/userdata /dev/block/userdataorig
 /sbin/bbx mv /dev/block/cache /dev/block/cacheorig
 
-#SLOT_LOC=$(/sbin/bbx cat /ss/safestrap/active_slot)
-
-# setup loopbacks
-/sbin/bbx losetup /dev/block/loop-system /ss/safestrap/rom-slot1/system.img
-/sbin/bbx losetup /dev/block/loop-userdata /ss/safestrap/rom-slot1/userdata.img
-/sbin/bbx losetup /dev/block/loop-cache /ss/safestrap/rom-slot1/cache.img
-
 # change symlinks
 /sbin/bbx ln -s /dev/block/loop-system /dev/block/system
 /sbin/bbx ln -s /dev/block/loop-userdata /dev/block/userdata
 /sbin/bbx ln -s /dev/block/loop-cache /dev/block/cache
+else
+/sbin/bbx umount /ss
+fi
 
-# LOG HERE
-/sbin/bbx ls -l /dev/block/* > /ss/block-dir.txt
-/sbin/bbx sync
-
+/sbin/bbx touch /.safestrapped
