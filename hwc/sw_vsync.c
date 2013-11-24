2a32d24d38d525aa13d0197497dbc62ee526e62a
Michael Gernoth <michael@gernoth.net>
2013-11-24 15:29:13 +0100

    fix SIGFPE in hwcomposer caused by modulo by zero
    Change-Id: I36e798dc01a54340c8014a23be0081443b62ef70

--- hwc/sw_vsync.c
+++ hwc/sw_vsync.c
@@ -61,6 +61,8 @@ static void *vsync_loop(void *data)
     for (;;) {
         pthread_mutex_lock(&vsync_mutex);
         period = vsync_rate; /* re-read rate */
+        if (period <= 0)
+            period = 1000000000 / 60;
         while (!vsync_loop_active) {
             pthread_cond_wait(&vsync_cond, &vsync_mutex);
         }
