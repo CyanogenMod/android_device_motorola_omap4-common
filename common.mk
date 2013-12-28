a869002373314e372934c274e3f0c9aa0a2d3055
Lucian Cristian <lucian.cristian@gmail.com>
2013-12-29 01:23:00 +0200

    Enable BLE
    Change-Id: Ifa5308ed5bcd07ea77ecd05ae5f7e0fad2da32a9

--- common.mk
+++ common.mk
@@ -134,6 +134,7 @@ PRODUCT_PACKAGES += \
 # Permissions files
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
