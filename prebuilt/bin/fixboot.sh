6f60933ea6ed9f50bae10db648d5523dd8534ffb
Michael Gernoth <michael@gernoth.net>
2014-01-26 00:09:20 +0100

    omap4-common: /ss does not need to be world read-/writable
    Change-Id: I6a9fd9e95b9b8f813961eb2c5704cf65b11a7426

--- prebuilt/bin/fixboot.sh
+++ prebuilt/bin/fixboot.sh
@@ -6,7 +6,7 @@
 /sbin/bbx chmod 777 /ss
 
 # mount safestrap partition
-/sbin/bbx mount -t vfat -o uid=1023,gid=1023 /dev/block/emstorage /ss
+/sbin/bbx mount -t vfat -o uid=1023,gid=1023,fmask=0007,dmask=0007,allow_utime=0020 /dev/block/emstorage /ss
 
 SLOT_LOC=$(/sbin/bbx cat /ss/safestrap/active_slot)
 
