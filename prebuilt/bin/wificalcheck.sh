da7bd2a1a9c442d4f210f6afb4700b0592a54936
Michael Gernoth <michael@gernoth.net>
2014-02-22 19:10:50 +0100

    omap4-common: fix wifi/p2p-config
    use right config-file for wifi and p2p and enable p2p on the
    p2p-interface
    
    Change-Id: I3add89572ebd3d14bd1d56b77702dac7bf64184f

--- prebuilt/bin/wificalcheck.sh
+++ prebuilt/bin/wificalcheck.sh
@@ -1,8 +1,22 @@
 #! /system/bin/sh
 # In some cases calibration is getting interupted before it finishes
 
-if [ ! -f /system/etc/firmware/ti-connectivity/wl1271-nvs.bin ];
+if [ ! -f /system/etc/firmware/ti-connectivity/wl1271-nvs.bin -o ! -s /system/etc/firmware/ti-connectivity/wl1271-nvs.bin ];
     then
         rmmod wl12xx_sdio
+
+        # Migrate config
+        if [ -e /data/misc/wifi/p2p_supplicant.conf -a -e /data/misc/wifi/wpa_supplicant.conf ];
+            then
+                if ! grep -q '^network' /data/misc/wifi/wpa_supplicant.conf &&
+                     grep -q '^manufacturer=TI' /data/misc/wifi/wpa_supplicant.conf &&
+                     grep -q '^network' /data/misc/wifi/p2p_supplicant.conf &&
+                   ! grep -q '^manufacturer=TI' /data/misc/wifi/p2p_supplicant.conf &&
+                     grep -q '^p2p_disabled=1' /data/misc/wifi/p2p_supplicant.conf;
+                    then
+                        mv /data/misc/wifi/p2p_supplicant.conf /data/misc/wifi/wpa_supplicant.conf
+                fi
+        fi
+
         wifical.sh
 fi
