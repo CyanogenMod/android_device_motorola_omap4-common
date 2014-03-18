eff6d746e345eafaec9c7755a90ca2f137e795ad
Michael Gernoth <michael@gernoth.net>
2014-03-18 08:20:19 +0100

    omap4-common: don't spam the logfile when changing scaling governors
    only log a failed write to the boostpulse_fd once and then retry
    opening it, instead of logging the failed write for every touchscreen
    interaction.
    
    Change-Id: I8a2d0741819b477ba260a6ac44f3295ba865ab94

--- libpower/power.c
+++ libpower/power.c
@@ -155,6 +155,8 @@ static int boostpulse_open(struct omap_power_module *omap_device) {
                 ALOGE("Error opening %s: %s\n", BOOSTPULSE_PATH, buf);
                 omap_device->boostpulse_warned = 1;
             }
+        } else {
+            omap_device->boostpulse_warned = 0;
         }
     }
 
@@ -192,6 +194,8 @@ static void omap_power_hint(struct power_module *module, power_hint_t hint, void
             if (len < 0) {
                 strerror_r(errno, buf, sizeof(buf));
                 ALOGE("Error writing to %s: %s\n", BOOSTPULSE_PATH, buf);
+                close(omap_device->boostpulse_fd);
+                omap_device->boostpulse_fd = -1;
             }
         }
         break;
