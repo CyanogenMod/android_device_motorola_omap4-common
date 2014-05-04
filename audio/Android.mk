38669168312f7488e4b7faaafc97bef25a0af89d
Michael Gernoth <michael@gernoth.net>
2014-05-04 12:18:05 +0200

    omap4-common: wrap Moto's proprietary audio-HAL
    Moto's audio-HAL is internally resampling when playing sound
    via bluetooth SCO (voice dialer, google now). When doing so,
    the returned value of written bytes is the arbitrary number
    192 when in fact all samples have been written.
    
    Wrap Motorola's audio-HAL to overload write() and catch
    that scenario and fix the return-value.
    
    Also prevent changing of device-settings when a read/write
    is in progress, as this might cause the operation getting
    stuck when the device is disabled at the wrong moment.
    
    This should fix sporadic audio-losses caused by changing
    mixer-settings while playiong/recording.
    
    Change-Id: I8d2e27f67f224dfb48f00801a64c4586b22c70ac

--- audio/Android.mk
+++ audio/Android.mk
@@ -37,3 +37,22 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+###
+### Wrapper for Motorola's HAL
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := audio.primary.$(TARGET_DEVICE)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SRC_FILES := wrapper.c
+
+LOCAL_C_INCLUDES += \
+	system/media/audio_utils/include \
+	system/media/audio_effects/include
+LOCAL_SHARED_LIBRARIES := liblog libcutils libaudioutils libdl
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
