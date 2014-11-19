9d9fb000f365db7b3f3d5e75707dd48c222ecc1a
Michael Gernoth <michael@gernoth.net>
2014-11-19 10:36:48 +0100

    kernel: don't use a special toolchain
    Change-Id: Id4784348fd16bf7ff90222c6f1cfcf68755a65f6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,7 +68,6 @@ TARGET_KRAIT_BIONIC_BBTHRESH := 64
 TARGET_KRAIT_BIONIC_PLDSIZE := 64
 
 # Kernel/Module Build
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 TARGET_KERNEL_SOURCE := kernel/motorola/omap4-common
 TARGET_KERNEL_CONFIG := mapphone_mmi_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := mapphone_mmi_selinux_defconfig
