ffbdb3c90d34fcfd9d07ffadc02484f4de63d9df
Michael Gernoth <michael@gernoth.net>
2015-01-25 19:57:06 +0100

    motoOmap4RIL: figure out correct prefixlen for radio if
    The Motorola RIL tries to set up a point-to-point interface which is no
    longer correctly supported on L. Figure out the correct netmask/
    prefixlen and change the interface-config so the gateway is actually
    reachable. The currently used kernel hack configures a static
    prefixlen of /30, which is not always correct.
    
    Change-Id: I48e50f7f5f97305580b901921cc5347fec5e9bf9

--- ril/telephony/java/com/android/internal/telephony/motoOmap4RIL.java
+++ ril/telephony/java/com/android/internal/telephony/motoOmap4RIL.java
@@ -26,26 +26,44 @@ import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
+import android.os.IBinder;
+import android.os.INetworkManagementService;
+import android.os.ServiceManager;
 import android.telephony.SmsMessage;
 import android.os.SystemProperties;
 import android.os.SystemClock;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.telephony.Rlog;
+import android.net.NetworkUtils;
+import android.net.InterfaceConfiguration;
+import android.net.LinkAddress;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
 
 import com.android.internal.telephony.RILConstants;
 import com.android.internal.telephony.gsm.SmsBroadcastConfigInfo;
 import com.android.internal.telephony.cdma.CdmaSmsBroadcastConfigInfo;
+import com.android.internal.telephony.dataconnection.DataCallResponse;
 
 public class motoOmap4RIL extends RIL implements CommandsInterface {
 
+    private INetworkManagementService mNwService;
+
     public motoOmap4RIL(Context context, int preferredNetworkType, int cdmaSubscription) {
         super(context, preferredNetworkType, cdmaSubscription);
+
+        IBinder b = ServiceManager.getService(Context.NETWORKMANAGEMENT_SERVICE);
+        mNwService = INetworkManagementService.Stub.asInterface(b);
     }
 
     public motoOmap4RIL(Context context, int preferredNetworkType,
                     int cdmaSubscription, Integer instanceId) {
         super(context, preferredNetworkType, cdmaSubscription, instanceId);
+
+        IBinder b = ServiceManager.getService(Context.NETWORKMANAGEMENT_SERVICE);
+        mNwService = INetworkManagementService.Stub.asInterface(b);
     }
 
     @Override
@@ -129,6 +147,9 @@ public class motoOmap4RIL extends RIL implements CommandsInterface {
     public void setDataAllowed(boolean allowed, Message result) {
         Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: setDataAllowed");
 
+        Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: faking VoiceNetworkState");
+        mVoiceNetworkStateRegistrants.notifyRegistrants(new AsyncResult(null, null, null));
+
         if (result != null) {
             CommandException e = new CommandException(CommandException.Error.REQUEST_NOT_SUPPORTED);
             AsyncResult.forMessage(result, null, e);
@@ -145,5 +166,131 @@ public class motoOmap4RIL extends RIL implements CommandsInterface {
             AsyncResult.forMessage(response, null, e);
             response.sendToTarget();
         }
+    }
+
+    @Override
+    public void setupDataCall(String radioTechnology, String profile, String apn,
+            String user, String password, String authType, String protocol,
+            Message result) {
+        Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: setupDataCall");
+
+        Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: faking VoiceNetworkState");
+        mVoiceNetworkStateRegistrants.notifyRegistrants(new AsyncResult(null, null, null));
+
+        super.setupDataCall(radioTechnology, profile, apn, user, password, authType, protocol, result);
+    }
+
+    @Override
+    protected DataCallResponse getDataCallResponse(Parcel p, int version) {
+        DataCallResponse dataCall = new DataCallResponse();
+        int dataPosition = p.dataPosition();
+
+        Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: getDataCallRespone");
+
+        if (version >= 5) {
+            dataCall.status = p.readInt();
+            dataCall.suggestedRetryTime = p.readInt();
+            dataCall.cid = p.readInt();
+            dataCall.active = p.readInt();
+            dataCall.type = p.readString();
+            dataCall.ifname = p.readString();
+            String addrs = p.readString();
+            String dnses = p.readString();
+            String gws = p.readString();
+
+            String addresses[] = null;
+            if (addrs != null) {
+                Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: Addr: " + addrs);
+                addresses = addrs.split(" ");
+            }
+
+            String gateways[] = null;
+            if (gws != null) {
+                Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: GW: " + gws);
+                gateways = gws.split(" ");
+            }
+
+            if (dataCall.ifname != null) {
+                Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: IF: " + dataCall.ifname);
+            }
+
+            InetAddress addr = null;
+            InetAddress gw = null;
+
+            if (addresses != null && addresses.length > 0) {
+                for (String a : addresses) {
+                    a = a.trim();
+                    if (a.isEmpty()) continue;
+
+                    try {
+                        addr = NetworkUtils.numericToInetAddress(a);
+                    } catch (Exception e) {
+                        Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: can't parse " + a);
+                    }
+
+                    if (addr != null && (addr instanceof Inet4Address)) {
+                        break;
+                    } else {
+                        addr = null;
+                    }
+                }
+            }
+
+            if (gateways != null && gateways.length > 0) {
+                for (String a : gateways) {
+                    a = a.trim();
+                    if (a.isEmpty()) continue;
+
+                    try {
+                        gw = NetworkUtils.numericToInetAddress(a);
+                    } catch (Exception e) {
+                        Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: can't parse " + a);
+                    }
+
+                    if (gw != null && (gw instanceof Inet4Address)) {
+                        break;
+                    } else {
+                        gw = null;
+                    }
+                }
+            }
+
+            if (addr != null && gw != null && dataCall.ifname != null && (!dataCall.ifname.equals("rmnet0"))) {
+                Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: Addr: " + addr.getHostAddress() + " GW: " + gw.getHostAddress() + " IF: " + dataCall.ifname);
+                Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: Searching for common prefix...");
+
+                for (int i = 30; i > 0; i--) {
+                    if (NetworkUtils.getNetworkPart(addr, i).getHostAddress().equals(
+                        NetworkUtils.getNetworkPart(gw, i).getHostAddress())) {
+                        Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: found prefixlen: " + i);
+
+                        InterfaceConfiguration ifcg = null;
+
+                        try {
+                            ifcg = mNwService.getInterfaceConfig(dataCall.ifname);
+                            if (ifcg != null) {
+                                LinkAddress la = new LinkAddress(addr, i);
+                                if ((ifcg.getLinkAddress() != null) && ifcg.getLinkAddress().isSameAddressAs(la)) {
+                                    Rlog.v(RILJ_LOG_TAG, "motoOmap4RIL: interface " + dataCall.ifname + " is already configured correctly");
+                                } else {
+                                    ifcg.setLinkAddress(la);
+                                    ifcg.setInterfaceUp();
+
+                                    mNwService.setInterfaceConfig(dataCall.ifname, ifcg);
+                                }
+                            } else {
+                                Rlog.e(RILJ_LOG_TAG, "motoOmap4RIL: can't get old config for interface " + dataCall.ifname);
+                            }
+                        } catch (Exception e) {
+                            Rlog.e(RILJ_LOG_TAG, "motoOmap4RIL: can't add address with correct prefix: " + e);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        p.setDataPosition(dataPosition);
+        return super.getDataCallResponse(p, version);
     }
 }
