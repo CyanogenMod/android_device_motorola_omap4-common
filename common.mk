252be4d7a0383e619fa30e7bf848d735cea3e9f7
Michael Gernoth <michael@gernoth.net>
2015-06-24 11:36:33 +0200

    omap4-common: remove explicit copy of media_codecs_ffmpeg.xml
    This is no longer needed, the file is now always copied.
    
    Change-Id: Idfedee8ac004bc0be5e3e1cb7a81ddcfab19bcf7

--- common.mk
+++ common.mk
@@ -253,8 +253,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # OpenglES
 PRODUCT_PROPERTY_OVERRIDES += \
