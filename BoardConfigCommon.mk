b49d8a5d64a6dfefea104d1d39159ce59dfc5fb0
Michael Gernoth <michael@gernoth.net>
2015-10-12 16:38:30 +0200

    BoardConfigCommon: remove BOARD_SEPOLICY_UNION
    Change-Id: Id54522a1459e25a76dd13c4fff056e03bcb17f27

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -181,33 +181,3 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.omap4
 
 BOARD_SEPOLICY_DIRS += \
     device/motorola/omap4-common/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    akmd8975.te \
-    battd.te \
-    bluetooth.te \
-    debuggered.te \
-    drmserver.te \
-    file_contexts \
-    device.te \
-    dhcp.te \
-    domain.te \
-    file.te \
-    init.te \
-    init_shell.te \
-    mediaserver.te \
-    modemlog.te \
-    motorild.te \
-    msp430.te \
-    netd.te \
-    property.te \
-    pvrsrvinit.te \
-    rild.te \
-    smc.te \
-    system.te \
-    tee.te \
-    thermal_manager.te \
-    uim.te \
-    vold.te \
-    whisperd.te \
-    wrigleyd.te
