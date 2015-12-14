a084afe0fb6e9159ab0321099b796afa74aa3824
Michael Gernoth <michael@gernoth.net>
2015-12-14 09:42:19 +0100

    motoaudiocompat: don't provide shims for reservedVectorImpl symbols
    Change-Id: I61a327132c9e1289a051ac3102b0fa05fedabc4d

--- audio/motoaudiocompat.c
+++ audio/motoaudiocompat.c
@@ -29,23 +29,3 @@ audio_policy_dev_state_t _ZN7android11AudioSystem24getDeviceConnectionStateE15au
 {
     return _ZN7android11AudioSystem24getDeviceConnectionStateEjPKc(device, device_address);
 }
-
-void _ZN7android10VectorImpl19reservedVectorImpl1Ev() { }
-void _ZN7android10VectorImpl19reservedVectorImpl2Ev() { }
-void _ZN7android10VectorImpl19reservedVectorImpl3Ev() { }
-void _ZN7android10VectorImpl19reservedVectorImpl4Ev() { }
-void _ZN7android10VectorImpl19reservedVectorImpl5Ev() { }
-void _ZN7android10VectorImpl19reservedVectorImpl6Ev() { }
-void _ZN7android10VectorImpl19reservedVectorImpl7Ev() { }
-void _ZN7android10VectorImpl19reservedVectorImpl8Ev() { }
-void _ZN7android10VectorImpl19reservedVectorImpl9Ev() { }
-
-void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl1Ev() { }
-void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl2Ev() { }
-void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl3Ev() { }
-void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl4Ev() { }
-void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl5Ev() { }
-void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl6Ev() { }
-void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl7Ev() { }
-void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl8Ev() { }
-void _ZN7android16SortedVectorImpl25reservedSortedVectorImpl9Ev() { }
