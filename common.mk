5ccf8996e7e8624090967510736dbe0b1af3ec00
Michael Gernoth <michael@gernoth.net>
2016-05-09 23:46:12 +0200

    camera: disable qcom features
    They don't work on Omap and will just cause strange behaviour
    (e.g. disabled focus mode)
    
    Change-Id: I7416747e5033dd0616c7ad0f9aec8edf17fdbf1f

--- common.mk
+++ common.mk
@@ -261,6 +261,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.media.panorama.defres=3264x1840 \
     ro.media.panorama.frameres=1280x720 \
     camera2.portability.force_api=1 \
+    camera.qcom.misc.disable=1 \
     media.stagefright.cache-params=18432/20480/15 \
     media.aac_51_output_enabled=true
 
